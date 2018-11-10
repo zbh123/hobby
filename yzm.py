6a3222429e154cbcf7ac9182e3c8d2ee60eeb1be
zbh123 <43636193+zbh123@users.noreply.github.com>
2018-11-10 16:25:22 +0800

    Create yzm.py

---
+++ yzm.py
@@ -0,0 +1,18 @@
+import random
+
+# 随机数
+print(random.random())              # 返回一个随机小数'0.4800545746046827'
+print(random.randint(1,5))          # 返回（1-5）随机整型数据
+print(random.randrange(1,10))       # 返回（1-10）随机数据
+
+# 生成随机验证码
+code = ''
+for i in range(4):
+    code += str(random.randint(0,9))
+    # current = random.randrange(0,4)
+    # if current != i:
+    #     temp = chr(random.randint(65,90))
+    # else:
+    #     temp = random.randint(0,9)
+    # code += str(temp)
+print (code)
