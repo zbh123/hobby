396ba2aa6e9684c930f02513ebbc873080c02c18
zbh123 <43636193+zbh123@users.noreply.github.com>
2019-07-01 08:37:38 +0800

    Update excute_c.py

--- UI/tomo/excute_c.py
+++ UI/tomo/excute_c.py
@@ -3,13 +3,13 @@
 QThread下不能再使用多进程，否则会跳出多个窗口，无法正常运行，但可以另外引进函数或者类，执行多进程，配合pyqtSignal功能可以满足当前条件
 '''
 
-from PyQt4 import *
+from PyQt4.Qt import *
 import os
 
 class Worker(QThread):
     file_changed_signal = pyqtSignal(str)
     def __init__(self):
-        super(Worker, self).__init__()
+        super(Worker, self).__init__(parent=None)
         self.working = True
 
     def __del__(self):
