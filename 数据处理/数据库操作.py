72b70bc7bcafb7cdf83f4ee1d7b1f577ad9aeed9
zbh123 <43636193+zbh123@users.noreply.github.com>
2019-07-03 18:52:58 +0800

    Update 数据库操作.py

--- 数据处理/数据库操作.py
+++ 数据处理/数据库操作.py
@@ -1,5 +1,11 @@
 import MySQLdb
-
+'''
+left join on简介,首先确认要选取的内容即主框架select * from result where id=10， 其中left join左边的是基础库表，
+右边的是选取元素的库表，on 之后=左边的是基础库表的内容，即根据主框架能得到的数据，右边的是对应库表的内容，两者是同一性质在不能库表的体现
+然后，left join 。。。 on 可以多次使用
+select job.job_name, job.buildname, test.testname, test.id, starttime, endtime from result 
+left join job on result.id = job.id left join test on result.test_id = test.id where result.id=10
+'''
 
 class DataBase():
     def __init__(self, IP, USER, PASSWD, baseName):
